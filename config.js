639c896bf539204376d16f79016556dcb6195d46
vogialong <92168232+vogialong@users.noreply.github.com>
2021-10-09 10:27:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,5 +14,3 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
